cc06dfef9dfa2c5d9aa746ea481bf9aa5ed423d0
Buhai Darius <buhaidarius@gmail.com>
2021-01-19 14:42:50 +0200

    Removed unused files

--- Worker/temp_data.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISKANALYZER_TEMP_DATA_H
-#define DISKANALYZER_TEMP_DATA_H
-
-#include "../Shared/shared.h"
-
-struct file_details* new_file_details(char*);
-struct file_details* append_file(struct file_details *, struct file_details *);
-
-#endif //DISKANALYZER_TEMP_DATA_H
